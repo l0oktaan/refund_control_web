b88ca6c9ec18100edc31991351f44be519c9ee47
l0oktaan <looktaan.tc3@gmail.com>
2024-10-08 22:39:11 +0700

    refund control start

---
+++ src/views/HomeView.vue
@@ -0,0 +1,24 @@
+<template>
+  
+  <div>
+    <h1>{{ message }}</h1>
+    <input v-model="message" placeholder="Type something..." />
+    <button @click="clearMessage">&nbsp;Clear Message ผxx</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+
+export default defineComponent({
+  setup() {
+    const message = ref<string>('Hello, Vue 3 with TypeScript! x');
+
+    const clearMessage = () => {
+      message.value = '';
+    };
+
+    return { message, clearMessage };
+  },
+});
+</script>
